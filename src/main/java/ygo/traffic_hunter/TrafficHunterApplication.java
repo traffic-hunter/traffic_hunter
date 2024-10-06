e6478bfd6dfc9d677e95c16d539828e1e58571bd
swager253 <swager253@naver.com>
2024-10-06 16:58:43 +0900

    first commit

---
+++ src/main/java/ygo/traffic_hunter/TrafficHunterApplication.java
@@ -0,0 +1,13 @@
+package ygo.traffic_hunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficHunterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ygo/traffic_hunter/TrafficHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package ygo.traffic_hunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficHunterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
