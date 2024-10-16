574db22d2d9b4087829b8f74c2072b366d1c77ca
swager253 <swager253@naver.com>
2024-10-16 17:40:14 +0900

    (#1) test: 기능 변화로 인한 테스트 import 변경

--- java-agent/src/test/java/ygo/traffichunter/agent/TrafficHunterAgentTest.java
+++ java-agent/src/test/java/ygo/traffichunter/agent/TrafficHunterAgentTest.java
@@ -1,19 +1,34 @@
 package ygo.traffichunter.agent;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
 import org.junit.jupiter.api.Test;
 import ygo.TestExt;
+import ygo.traffichunter.agent.engine.jvm.JVMSelector;
 
 class TrafficHunterAgentTest extends TestExt {
 
     @Test
-    void agent를_테스트한다() {
+    void 타겟_jvm_actuator를_에이전트_jmx로_연결하여_메트릭을_수집한다() throws Exception {
         // given
-
+        JMXServiceURL jmxUrl = JVMSelector.getVM("ygo.testapp.TestAppApplication");
 
         // when
+        JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxUrl);
+
+        MBeanServerConnection mbsc = jmxConnector.getMBeanServerConnection();
+
+        ObjectName objectName = new ObjectName("org.springframework.boot:type=Endpoint,name=Metrics");
+
+        Object health = mbsc.invoke(objectName, "metric", new Object[] {"jvm.memory.used"}, new String[] {String.class.getName()});
 
         // then
-
+        System.out.println(health);
     }
 }
--- java-agent/src/test/java/ygo/traffichunter/http/HttpBuilderTest.java
+++ java-agent/src/test/java/ygo/traffichunter/http/HttpBuilderTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.net.URI;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import ygo.traffichunter.agent.engine.systeminfo.SystemInfo;
 import ygo.traffichunter.agent.engine.systeminfo.cpu.CpuStatusInfo;
+import ygo.traffichunter.agent.engine.systeminfo.gc.GarbageCollectionStatusInfo;
 import ygo.traffichunter.agent.engine.systeminfo.memory.MemoryStatusInfo;
 import ygo.traffichunter.agent.engine.systeminfo.memory.MemoryStatusInfo.MemoryUsage;
+import ygo.traffichunter.agent.engine.systeminfo.runtime.RuntimeStatusInfo;
 import ygo.traffichunter.agent.engine.systeminfo.thread.ThreadStatusInfo;
 
 class HttpBuilderTest {
@@ -20,7 +23,9 @@ class HttpBuilderTest {
         SystemInfo systemInfo = new SystemInfo(
                 new MemoryStatusInfo(new MemoryUsage(1, 1, 1, 1), new MemoryUsage(1,1, 1, 1)),
                 new ThreadStatusInfo(1,1, 1),
-                new CpuStatusInfo(1, 1, 1)
+                new CpuStatusInfo(1, 1, 1),
+                new GarbageCollectionStatusInfo(List.of()),
+                new RuntimeStatusInfo(1, 1, "", "")
         );
 
         // when
