ab35d369924c2e121871f686e8f2acb09b8a9446
swager253 <swager253@naver.com>
2024-10-29 22:45:59 +0900

    (#6) metric 수집 리팩토링

---
+++ java-agent/src/main/java/ygo/traffichunter/agent/engine/systeminfo/TransactionInfo.java
@@ -0,0 +1,23 @@
+package ygo.traffichunter.agent.engine.systeminfo;
+
+import java.time.Instant;
+
+public record TransactionInfo(
+        String txName,
+        Instant startTime,
+        Instant endTime,
+        long duration,
+        String errorMessage,
+        boolean isSuccess
+) {
+
+    public static TransactionInfo create(final String txName,
+                                         final Instant startTime,
+                                         final Instant endTime,
+                                         final long duration,
+                                         final String errorMessage,
+                                         final boolean isSuccess) {
+
+        return new TransactionInfo(txName, startTime, endTime, duration, errorMessage, isSuccess);
+    }
+}
