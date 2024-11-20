790c04aa4907c2c5ac95c715d8ffa690ee2f3b59
swager253 <swager253@naver.com>
2024-11-20 15:58:00 +0900

    (#13) measurement 스펙 변경

--- server/src/main/java/ygo/traffic_hunter/dto/measurement/metadata/Metadata.java
+++ server/src/main/java/ygo/traffic_hunter/dto/measurement/metadata/Metadata.java
@@ -1,18 +1,24 @@
 package ygo.traffic_hunter.dto.measurement.metadata;
 
+import com.influxdb.annotations.Column;
 import java.time.Instant;
 import ygo.traffic_hunter.dto.systeminfo.metadata.AgentStatus;
 
 public record Metadata(
 
+        @Column(name = "agent_id", tag = true)
         String agentId,
 
+        @Column(name = "agent_name", tag = true)
         String agentName,
 
+        @Column(name = "agent_version")
         String agentVersion,
 
+        @Column(name = "agent_boot_time")
         Instant agentBootTime,
 
+        @Column(name = "agent_status")
         AgentStatus agentStatus
 ) {
 }
