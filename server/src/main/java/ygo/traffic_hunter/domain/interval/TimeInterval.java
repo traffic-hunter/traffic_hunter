24a8f8ecb011b186de11d75c328b9cff5aa7f8a1
swager253 <swager253@naver.com>
2024-11-27 16:15:12 +0900

    (#13) interval 시간 추가

--- server/src/main/java/ygo/traffic_hunter/domain/interval/TimeInterval.java
+++ server/src/main/java/ygo/traffic_hunter/domain/interval/TimeInterval.java
@@ -5,7 +5,17 @@ import lombok.Getter;
 @Getter
 public enum TimeInterval {
 
+    REAL_TIME("3 seconds"),
+    FIVE_MINUTES("5 minutes"),
     TEN_MINUTES("10 minutes"),
+    THIRTY_MINUTES("30 minutes"),
+    ONE_HOUR("1 hours"),
+    THREE_HOURS("3 hours"),
+    SIX_HOURS("6 hours"),
+    TWELVE_HOURS("12 hours"),
+    ONE_DAYS("1 days"),
+    TWO_DAYS("2 days"),
+    THREE_DAYS("3 days"),
     ;
 
     private final String interval;
