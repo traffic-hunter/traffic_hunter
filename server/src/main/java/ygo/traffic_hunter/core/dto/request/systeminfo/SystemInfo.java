04bf1fdd57c60c31823577f8456c821c4d50f228
swager253 <swager253@naver.com>
2024-11-28 17:11:58 +0900

    (#13) tomcat, dbcp 메트릭 추가

--- server/src/main/java/ygo/traffic_hunter/core/dto/request/systeminfo/SystemInfo.java
+++ server/src/main/java/ygo/traffic_hunter/core/dto/request/systeminfo/SystemInfo.java
@@ -2,11 +2,13 @@ package ygo.traffic_hunter.core.dto.request.systeminfo;
 
 import java.time.Instant;
 import ygo.traffic_hunter.core.dto.request.Metric;
+import ygo.traffic_hunter.core.dto.request.systeminfo.dbcp.HikariDbcpInfo;
 import ygo.traffic_hunter.core.dto.request.systeminfo.gc.GarbageCollectionStatusInfo;
 import ygo.traffic_hunter.core.dto.request.systeminfo.cpu.CpuStatusInfo;
 import ygo.traffic_hunter.core.dto.request.systeminfo.memory.MemoryStatusInfo;
 import ygo.traffic_hunter.core.dto.request.systeminfo.runtime.RuntimeStatusInfo;
 import ygo.traffic_hunter.core.dto.request.systeminfo.thread.ThreadStatusInfo;
+import ygo.traffic_hunter.core.dto.request.systeminfo.web.tomcat.TomcatWebServerInfo;
 
 public record SystemInfo(
         Instant time,
@@ -14,5 +16,7 @@ public record SystemInfo(
         ThreadStatusInfo threadStatusInfo,
         CpuStatusInfo cpuStatusInfo,
         GarbageCollectionStatusInfo garbageCollectionStatusInfo,
-        RuntimeStatusInfo runtimeStatusInfo
+        RuntimeStatusInfo runtimeStatusInfo,
+        TomcatWebServerInfo tomcatWebServerInfo,
+        HikariDbcpInfo hikariDbcpInfo
 ) implements Metric {}
