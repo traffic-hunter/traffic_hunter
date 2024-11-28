6181a191925be456793fd59ca1ad4dd0ba44b154
swager253 <swager253@naver.com>
2024-11-28 15:57:35 +0900

    (#6) tomcat, hikari 메트릭 추가

--- java-agent/src/main/java/ygo/traffichunter/agent/engine/collect/MetricCollectSupport.java
+++ java-agent/src/main/java/ygo/traffichunter/agent/engine/collect/MetricCollectSupport.java
@@ -7,32 +7,48 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ygo.traffichunter.agent.engine.collect.dbcp.hikari.HikariCPMetricCollector;
 import ygo.traffichunter.agent.engine.collect.systeminfo.cpu.CpuMetricCollector;
 import ygo.traffichunter.agent.engine.collect.systeminfo.gc.GarbageCollectionMetricCollector;
 import ygo.traffichunter.agent.engine.collect.systeminfo.memory.MemoryMetricCollector;
 import ygo.traffichunter.agent.engine.collect.systeminfo.runtime.RuntimeMetricCollector;
 import ygo.traffichunter.agent.engine.collect.systeminfo.thread.ThreadMetricCollector;
+import ygo.traffichunter.agent.engine.collect.web.tomcat.TomcatMetricCollector;
 import ygo.traffichunter.agent.engine.jvm.JVMSelector;
+import ygo.traffichunter.agent.engine.metric.dbcp.HikariDbcpInfo;
 import ygo.traffichunter.agent.engine.metric.systeminfo.SystemInfo;
+import ygo.traffichunter.agent.engine.metric.web.tomcat.TomcatWebServerInfo;
+import ygo.traffichunter.agent.property.TrafficHunterAgentProperty;
 
 public class MetricCollectSupport {
 
     private static final Logger log = LoggerFactory.getLogger(MetricCollectSupport.class);
 
-    private static final MemoryMetricCollector collectMemory = new MemoryMetricCollector();
+    private final MemoryMetricCollector collectorMemory;
 
-    private static final CpuMetricCollector collectCpu = new CpuMetricCollector();
+    private final CpuMetricCollector collectorCpu;
 
-    private static final ThreadMetricCollector collectThread = new ThreadMetricCollector();
+    private final ThreadMetricCollector collectorThread;
 
-    private static final GarbageCollectionMetricCollector collectorGC = new GarbageCollectionMetricCollector();
+    private final GarbageCollectionMetricCollector collectorGC;
 
-    private static final RuntimeMetricCollector collectorRuntime = new RuntimeMetricCollector();
+    private final RuntimeMetricCollector collectorRuntime;
 
-    private MetricCollectSupport() {
+    private final TomcatMetricCollector collectorTomcat;
+
+    private final HikariCPMetricCollector collectorHikari;
+
+    public MetricCollectSupport(final TrafficHunterAgentProperty property) {
+        this.collectorMemory = new MemoryMetricCollector();
+        this.collectorCpu = new CpuMetricCollector();
+        this.collectorThread = new ThreadMetricCollector();
+        this.collectorGC = new GarbageCollectionMetricCollector();
+        this.collectorRuntime = new RuntimeMetricCollector();
+        this.collectorTomcat = new TomcatMetricCollector(property);
+        this.collectorHikari = new HikariCPMetricCollector();
     }
 
-    public static SystemInfo collect(final String targetJVMPath) {
+    public SystemInfo collect(final String targetJVMPath) {
 
         try (final JMXConnector jmxConnector = JMXConnectorFactory.connect(JVMSelector.getVMXServiceUrl(targetJVMPath))) {
 
@@ -40,9 +56,9 @@ public class MetricCollectSupport {
 
             return new SystemInfo(
                     Instant.now(),
-                    collectMemory.collect(mbsc),
-                    collectThread.collect(mbsc),
-                    collectCpu.collect(mbsc),
+                    collectorMemory.collect(mbsc),
+                    collectorThread.collect(mbsc),
+                    collectorCpu.collect(mbsc),
                     collectorGC.collect(mbsc),
                     collectorRuntime.collect(mbsc)
             );
@@ -53,12 +69,20 @@ public class MetricCollectSupport {
         }
     }
 
-    public static SystemInfo collect() {
+    public TomcatWebServerInfo collectWebServer() {
+        return collectorTomcat.collect();
+    }
+
+    public HikariDbcpInfo collectDbcp() {
+        return collectorHikari.collect();
+    }
+
+    public SystemInfo collect() {
         return new SystemInfo(
                 Instant.now(),
-                collectMemory.collect(),
-                collectThread.collect(),
-                collectCpu.collect(),
+                collectorMemory.collect(),
+                collectorThread.collect(),
+                collectorCpu.collect(),
                 collectorGC.collect(),
                 collectorRuntime.collect()
         );
