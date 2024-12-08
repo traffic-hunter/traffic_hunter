16fdb39e54cf63f8b9dc5a4b25d85bbe448e3cc7
swager253 <swager253@naver.com>
2024-12-08 17:49:05 +0900

    (#18) 모듈에 포함된 빌드 파일 모두 로드

---
+++ test-app/src/main/java/ygo/testapp/TestAppApplication.java
@@ -0,0 +1,12 @@
+package ygo.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	}
+}
---
+++ test-app2/src/test/java/ygo/testapp2/TestApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ygo.testapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
