8afcdde50358132ef84532b18c31fc126911e832
swager253 <swager253@naver.com>
2025-01-05 16:21:29 +0900

    (#22) java-agent 모듈 삭제

--- java-agent/plugin/plugin-sdk/src/main/java/org/traffichunter/javaagent/plugin/sdk/instrumentation/PluginInstrumentation.java
+++
@@ -1,9 +0,0 @@
-package org.traffichunter.javaagent.plugin.sdk.instrumentation;
-
-import net.bytebuddy.agent.builder.AgentBuilder;
-import org.traffichunter.javaagent.plugin.sdk.instrumentation.type.TypeInstrumentation;
-
-public interface PluginInstrumentation extends TypeInstrumentation {
-
-    void transform(AgentBuilder.Transformer transformer, ClassLoader classLoader);
-}
--- java-agent/plugin/plugin-sdk/src/main/java/org/traffichunter/javaagent/plugin/sdk/loader/PluginLoader.java
+++
@@ -1,8 +0,0 @@
-package org.traffichunter.javaagent.plugin.sdk.loader;
-
-import java.util.List;
-
-public interface PluginLoader<P> {
-
-    List<P> loadModules(ClassLoader classLoader);
-}
