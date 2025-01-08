40dfc3a718438c0755c6ea5d5d9727cdcedf939f
swager253 <swager253@naver.com>
2025-01-09 00:38:01 +0900

    [no issue] schema 호환성 문제 해결

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,6 +1,7 @@
+CREATE SEQUENCE if not exists agent_id START 1;
 CREATE TABLE if not exists agent
 (
-    id              INTEGER                     NOT NULL,
+    id              INTEGER                     DEFAULT nextval('agent_id') NOT NULL,
     agent_id        VARCHAR                     NOT NULL,
     agent_name      VARCHAR                     NOT NULL,
     agent_version   VARCHAR                     NOT NULL,
@@ -9,33 +10,27 @@ CREATE TABLE if not exists agent
 );
 
 CREATE TABLE IF NOT EXISTS metric_measurement (
-                                    time timestamptz            not null,
-                                    agent_id varchar            not null,
-                                    agent_name varchar          not null,
-                                    agent_version varchar       not null,
-                                    agent_boot_time timestamptz not null,
-                                    metric_data jsonb           not null
+                                                  time timestamptz            not null,
+                                                  metric_data jsonb           not null,
+                                                  agent_id integer            not null
 );
 
 CREATE TABLE IF NOT EXISTS transaction_measurement (
-                                         time timestamptz            not null,
-                                         agent_id varchar            not null,
-                                         agent_name varchar          not null,
-                                         agent_version varchar       not null,
-                                         agent_boot_time timestamptz not null,
-                                         transaction_data jsonb      not null
+                                                       time timestamptz            not null,
+                                                       transaction_data jsonb      not null,
+                                                       agent_id integer            not null
 );
 
 SELECT create_hypertable(
                'metric_measurement',
                by_range('time'),
-                if_not_exists := TRUE
+               if_not_exists := TRUE
        );
 
 SELECT create_hypertable(
                'transaction_measurement',
                by_range('time'),
-                if_not_exists := TRUE
+               if_not_exists := TRUE
        );
 
 CREATE INDEX IF NOT EXISTS metric_measurement_agent_id_time_idx ON metric_measurement (agent_id, time);
