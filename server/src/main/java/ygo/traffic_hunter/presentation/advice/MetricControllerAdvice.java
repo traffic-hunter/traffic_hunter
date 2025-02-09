315ff8c491eeee3ad861719efe6401177b99ebe9
swager253 <swager253@naver.com>
2025-02-09 17:20:48 +0900

    [no-issue] ErrorResponse static factory method 로 변경

--- server/src/main/java/ygo/traffic_hunter/presentation/advice/MetricControllerAdvice.java
+++ server/src/main/java/ygo/traffic_hunter/presentation/advice/MetricControllerAdvice.java
@@ -26,12 +26,11 @@ package ygo.traffic_hunter.presentation.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ServerErrorException;
 import ygo.traffic_hunter.common.exception.TrafficHunterException;
 
 /**
@@ -45,24 +44,30 @@ public class MetricControllerAdvice {
     @ExceptionHandler(TrafficHunterException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleTrafficHunterException(final TrafficHunterException e) {
-        return new ServerErrorException(e.getMessage(), e);
+        return ErrorResponse.create(e, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleRuntimeException(final RuntimeException e) {
-        return new ServerErrorException(e.getMessage(), e);
+        return ErrorResponse.create(e, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException e) {
-        return new ErrorResponseException(HttpStatus.BAD_REQUEST, e);
+        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalStateException(final IllegalStateException e) {
-        return new ErrorResponseException(HttpStatus.BAD_REQUEST, e);
+        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
