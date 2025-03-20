eccec745679cb4dc301e5c9fc4c0c467426dfe55
swager253 <swager253@naver.com>
2025-03-20 14:53:00 +0900

    [no-issue] feat: java-agent http-url-connection plugin

---
+++ java-apm-agent/plugin/http-url-connection/src/main/java/org/traffichunter/javaagent/plugin/http/HttpUrlConnectionPluginInstrumentation.java
@@ -0,0 +1,123 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2024 traffic-hunter.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.traffichunter.javaagent.plugin.http;
+
+import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.context.Context;
+import java.net.HttpURLConnection;
+import net.bytebuddy.asm.Advice.Enter;
+import net.bytebuddy.asm.Advice.FieldValue;
+import net.bytebuddy.asm.Advice.Local;
+import net.bytebuddy.asm.Advice.OnMethodEnter;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.asm.Advice.This;
+import net.bytebuddy.asm.Advice.Thrown;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.traffichunter.javaagent.extension.AbstractPluginInstrumentation;
+import org.traffichunter.javaagent.extension.Transformer;
+import org.traffichunter.javaagent.plugin.sdk.CallDepth;
+import org.traffichunter.javaagent.plugin.sdk.instumentation.SpanScope;
+
+/**
+ * @author yungwang-o
+ * @version 1.1.0
+ */
+@AutoService(AbstractPluginInstrumentation.class)
+public class HttpUrlConnectionPluginInstrumentation extends AbstractPluginInstrumentation {
+
+    public HttpUrlConnectionPluginInstrumentation() {
+        super("http-url-connection", HttpUrlConnectionPluginInstrumentation.class.getName(), "");
+    }
+
+    @Override
+    public void transform(final Transformer transformer) {
+        transformer.processAdvice(
+                Advices.create(
+                        ElementMatchers.isMethod()
+                                .and(isPublic())
+                                .and(namedOneOf("connect", "getOutputStream", "getInputStream")),
+                        HttpUrlConnectionAdvice.class
+                )
+        );
+    }
+
+    @Override
+    public ElementMatcher<? super TypeDescription> typeMatcher() {
+        return nameStartsWith("java.net")
+                .or(nameStartsWith("sun.net"))
+                .or(named("weblogic.net.http.HttpURLConnection"))
+                .and(not(named("sun.net.www.protocol.https.HttpsURLConnectionImpl")))
+                .and(hasSuperType(named("java.net.HttpURLConnection")));
+    }
+
+    @Override
+    protected ElementMatcher<? super MethodDescription> isMethod() {
+        return null;
+    }
+
+    public static class HttpUrlConnectionAdvice {
+
+        @OnMethodEnter(suppress = Throwable.class)
+        public static SpanScope enter(@This final HttpURLConnection httpURLConnection,
+                                      @FieldValue("connected") boolean connected,
+                                      @Local("callDepth") CallDepth callDepth) {
+
+            callDepth = CallDepth.forClass(HttpURLConnection.class);
+
+            if(callDepth.getAndIncrement() > 0) {
+                return null;
+            }
+
+            Context context = Context.current();
+
+            return HttpUrlConnectionInstrumentationHelper.start(httpURLConnection, connected, context);
+        }
+
+        @OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+        public static void exit(@Enter final SpanScope spanScope,
+                                @Thrown final Throwable throwable,
+                                @Local("callDepth") CallDepth callDepth) {
+
+            if(callDepth.getAndDecrement() > 0) {
+                return;
+            }
+
+            callDepth.getAndIncrement();
+
+            HttpUrlConnectionInstrumentationHelper.end(spanScope, throwable);
+
+            callDepth.getAndDecrement();
+        }
+    }
+}
