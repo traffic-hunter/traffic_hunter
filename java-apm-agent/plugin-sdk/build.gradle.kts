7317ee9822008d759b362922e721dd2eb07e7b4c
swager253 <swager253@naver.com>
2025-03-22 22:59:04 +0900

    [no-issue] feat: servlet plugin

--- java-apm-agent/plugin-sdk/build.gradle.kts
+++ java-apm-agent/plugin-sdk/build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
     implementation("io.opentelemetry:opentelemetry-api:1.45.0")
     implementation("io.opentelemetry:opentelemetry-api-incubator:1.45.0-alpha")
 }
