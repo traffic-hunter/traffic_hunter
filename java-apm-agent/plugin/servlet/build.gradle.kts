9e82de7a59f6573dbf8e79bb45d454f619323eee
swager253 <swager253@naver.com>
2025-03-25 15:34:06 +0900

    [no-issue] gradle dependencies clear

--- java-apm-agent/plugin/servlet/build.gradle.kts
+++ java-apm-agent/plugin/servlet/build.gradle.kts
@@ -10,9 +10,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
 }
 
